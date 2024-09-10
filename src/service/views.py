876d561511732c2f6de2e2488a545525ce5f8258
Naoki-02 <smile.naonao@icloud.com>
2024-09-10 22:08:38 +0900

    gpt_api 完了

--- src/service/views.py
+++ src/service/views.py
@@ -1,5 +1,7 @@
+import json
+
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import View
 from openai import OpenAI
@@ -7,19 +9,78 @@ from openai import OpenAI
 
 class ChatGPTView(View):
     def post(self, request, *args, **kwargs):
-        client=OpenAI(api_key=settings.OEPNAI_API_KEY)
+        # client = OpenAI(api_key=settings.OEPNAI_API_KEY)
         user_message = request.POST.get('message')
 
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": user_message},
-            ],
-        )
+        try:
 
-        gpt_response = response.choices[0].message['content']
-        return JsonResponse({'response': gpt_response})
+            # response = client.chat.completions.create(
+            #     model="gpt-3.5-turbo",
+            #     # 6. come up with three menus: breakfast, lunch, and dinner.
+            #     # "lunch": [{ ... }],
+            #     # "dinner": [{ ... }]
+            #     messages=[
+            #         {"role": "system", "content": """You are the assistant of the menu suggestion AI. Please suggest a nutritionally balanced menu using the given list of ingredients. Please return your response in JSON format according to the following criteria: 1: 1.
+            #             1. propose a one-soup, three-course menu that includes a main dish, a side dish, and a soup. 2.
+            #             2. use the specified ingredients as much as possible. 3.
+            #             3. for each dish, include the name of the dish, ingredients used, and brief cooking instructions.
+            #             4. include comments on nutritional balance.
+            #             5. use up all ingredients completely.
+            #             6. Responses must be returned in Japanese.
+            #             7. Always return the response in json format.
+            #             Generate a JSON response in the following format
+            #             {
+            #                 "menu": [
+            #                     "breakfast": [{
+            #                         "type": "main dish",
+            #                         "name": "name of dish",
+            #                         "ingredients": ["ingredient 1", "ingredient 2", ...],
+            #                         "instructions": "Brief description of the cooking procedure",
+            #                         },
+            #                         {
+            #                         "type": "side dish",
+            #                         "name": "name of dish",
+            #                         "ingredients": ["ingredient 1", "ingredient 2", ...],
+            #                         "instructions": "Brief description of the cooking procedure",
+            #                         },
+            #                         {
+            #                         "type": "soup", {
+            #                         "name“: "name of dish",
+            #                         "ingredients": ["ingredient 1", "ingredient 2", ...],
+            #                         "instructions": "Brief description of the cooking procedure"
+            #                     }],
+            #                 ],
+            #                 "nutritional_comment": "Comment on nutritional balance"
+            #             Using the given list of ingredients, please suggest a menu in the above format."""
+            #         },
+                    
+            #         {"role": "user", "content": user_message},
+            #     ],
+            # )
+            
+            # gpt_response = response.choices[0].message.content.strip()
+            # data = json.loads(gpt_response)
+            
+            try:
+                # JSONレスポンスをPythonの辞書に変換
+                with open('tmp.json','r') as f:
+                    data = json.load(f)
+                # json.dump関数を使ったJSONファイルへの書き出し
+                # with open('tmp.json', 'wt') as f:
+                    # json.dump(data, f)
+            except json.JSONDecodeError:
+                data = None
+            
+            if data is not None:
+                return JsonResponse({'response': data["menu"][0]["name"]}, status=200)
+            else:
+                return JsonResponse({'error': 'Invalid JSON'}, status=400)
+        # except client.error.RateLimitError:
+        #     return JsonResponse({'error': 'APIのクォータを超えました。しばらくしてから再試行してください。'}, status=429)
+        # except client.error.AuthenticationError:
+        #     return JsonResponse({'error': 'APIキーが無効です。設定を確認してください。'}, status=401)
+        except Exception as e:
+            return JsonResponse({'error': 'ChatGPT APIへのリクエスト中にエラーが発生しました。'}, status=500)
 
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
